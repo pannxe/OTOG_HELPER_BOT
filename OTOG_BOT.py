c2fbb54e0aeefa5c199ca520e82ca805bc0613ec
PondJaTH <palapon2545@gmail.com>
2020-03-24 23:39:25 +0700

    Random hello(), @tag message
    - Add Random text message on @tag and hello() function
    - @tag depend on self.user.name not bot name "OTOG_Helper"

--- OTOG_BOT.py
+++ OTOG_BOT.py
@@ -1,4 +1,5 @@
 import discord
+from random import randint
 from urllib.request import Request, urlopen
 req = Request('https://otog.cf/main', headers={'User-Agent': 'Mozilla/5.0'})
 
@@ -41,6 +42,14 @@ def Get_User_Ongoing():
 			return "ฮั่นแน่ มี "+e+" ที่ยังมีชีวิตอยู่"
 	return "ไม่มีอะ"
 
+def Get_Random_Text_forMention():
+
+    Words = ["จงทำโจทย์ จงทำโจทย์ จงทำโจทย์","ทำโจทย์เถอะ ขอหล่ะ","ว่างมากนั้นก็ไปทำโจทย์สิ","ไม่อ่าน ไม่ตอบ ไม่สน...","แต่ว่า...ทำโจทย์ด้วยสิ...",";w;","=A=!","- -*","แล้วไง?","https://giphy.com/gifs/sad-cry-capoo-3og0IG0skAiznZQLde","https://giphy.com/stickers/cat-pearl-capoo-TFUhSMPFJG7fPAiLpQ","https://giphy.com/gifs/happy-rainbow-capoo-XEgmzMLDhFQAga8umN","https://giphy.com/gifs/cat-color-capoo-dYZxsY7JIMSy2Afy6e","ระเบิดเวลา......**อ๊าาาาาาาาาาาาาาาาาาาาาาาาาาาาาาาาาาาา**","โฮ่... แทนที่แกจะเข้าค่ายอื่น แกกลับเดินมาค่ายคอม อย่างนั้นนะเรอะ","เปล่าประโยชน์ เปล่าประโยชน์ เปล่าประโยชน์ เปล่าประโยชน์","**How Dare You!!??**","เคยฟังรึเปล่า... X ติดไซเรน (https://pastebin.com/6a7u1b85)","พี่รู้ว่ามันเศร้า แต่จงทำโจทย์ต่อไปครับ","ญิรดีร์ฏ้อณรับสูเก็ฒเฎอร์ฌาวไฑญ",":thinking:",":joy:",":poop:",":+1:",":eyes:",":P"]
+    return Words[randint(0,len(Words)-1)]
+
+def Get_Random_Text_forHello():
+    Words = ["สวัสดีเจ้า","สวัสดีจ้า","สวัสดีครับ","สวัสดีค่ะ","ສະບາຍດີ","Annyeonghaseyo","Kon'nichiwa","Hello","привет!","ว่าไง",";w;?","Meow Meooww?",":wave:","https://giphy.com/gifs/capoo-halloween-3ov9k0OmfNYeLdK4gg","Nǐ hǎo"]
+    return Words[randint(0,len(Words)-1)] + " {0.author.mention}"
 
 
 class MyClient(discord.Client):
@@ -58,7 +67,7 @@ class MyClient(discord.Client):
 
 
 		if message.content.startswith('hello()'):
-			await message.channel.send('สวัสดีไอ้หน้า {0.author.mention}'.format(message))
+			await message.channel.send(Get_Random_Text_forHello().format(message))
 		if message.content.startswith('help()') or message.content.startswith('!help'):
 			em = discord.Embed(title = "สิ่งที่น้อมทำได้",description = "มีแค่นี้แหละ")
 			em.add_field(name = "help()",value = "ก็ที่ทำอยู่ตอนนี้แหละ")
@@ -88,10 +97,8 @@ class MyClient(discord.Client):
 
 
 		for Mem in message.mentions:
-			if "OTOG_Helper" == Mem.display_name:
-				await message.channel.send('ไม่อ่าน ไม่ตอบ ไม่สน...')
-				await message.channel.send('สนแค่คำสั่งที่ลงท้ายด้วย ()')
-				await message.channel.send('ลอง help() จะช่วยให้ชีวิตดีขึ้น :)')
+			if self.user.name == Mem.display_name:
+				await message.channel.send(Get_Random_Text_forMention())
 				break
 
 	async def on_guild_join(guild):
