f09bb73361ba8077451f709c54253089f9e36778
Krit Patrix <kzt.patrix@gmail.com>
2020-07-08 22:08:22 +0700

    :zap: Remove redundancy and improve structure.

--- Pat1Grader.py
+++ Pat1Grader.py
@@ -4,72 +4,57 @@ import signal
 import os
 import time
 
-PATH = os.path.realpath(__file__)
-while PATH[-1] != "\\":
-	PATH = PATH[:-1]
+PATH = f"{os.getcwd()}\\"
 
+def compare_equal(OUT):
+    with open(f"Pat1TestCase\\{i}.sol") as f:
+        SOL = f.read().strip().split("\n")
+    OUT = OUT.decode().strip().split("\n")
+    if len(OUT) != len(SOL):
+        return "-"
+    else:
+        for j in range(len(OUT)):
+            if OUT[i] != SOL[i]:
+                return "-"
+        return "P"
 
 def Grading(namae):
-	Now_FILE = "VerifyCode\\" +namae + ".cpp"
-	Now_EXE = "VerifyCode\\" +namae + "RUN"
+    Now_FILE = f"VerifyCode\\{namae}.cpp"
+    Now_EXE = f"VerifyCode\\{namae}RUN"
 
-	p = Popen(['g++', '-O2', PATH+Now_FILE,'-o', Now_EXE], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
-	(a,_) = p.communicate()
-	a = a.decode()
-	RET = p.returncode
-	if RET!= 0:
-		return "อ่อนหัด!! แค่นี้ก็ยัง ERROR\n" + a.replace(PATH,"..\\")
+    p = Popen(
+        ["g++", "-O2", PATH + Now_FILE, "-o", Now_EXE],
+        stdout=PIPE,
+        stdin=PIPE,
+        stderr=STDOUT,
+    )
+    (a, _) = p.communicate()
+    RET = p.returncode
 
-	Verdict = ""
-	sumTime = 0
-	per = True
-	for i in range(1,10+1):
-		startTime = time.time()
-		p = Popen([PATH+Now_EXE], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
-		try:
-			with open("Pat1TestCase\\" +str(i)+".in") as F:
-				OUT = p.communicate(timeout=2,input=F.read().encode())[0]
-			sumTime += time.time() - startTime
-			RET = p.returncode
-		except subprocess.TimeoutExpired:
-			sumTime += time.time() - startTime
-			p.terminate()
-			Verdict += "T"
-			per = False
-			continue
+    if RET != 0:
+        return "อ่อนหัด!! แค่นี้ก็ยัง ERROR\n" + a.decode().replace(PATH, "..\\")
 
-		p.terminate()
+    Verdict = ""
+    sumTime = 0
 
-		if RET != 0:
-			Verdict += "X"
-			per = False
-			continue
+    for i in range(1, 11):
+        startTime = time.time()
+        p = Popen([PATH + Now_EXE], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+        try:
+            with open(f"Pat1TestCase\\{i}.in") as F:
+                OUT = p.communicate(timeout=2, input=F.read().encode())[0]
+            	RET = p.returncode
+        except subprocess.TimeoutExpired:
+            RET = 124
+		
+        sumTime += time.time() - startTime
+        p.terminate()
 
+        if RET != 0:
+            Verdict += "T" if RET == 124 else "X"
+            continue
 
+        # Grading session
+        Verdict += compare_equal(OUT)
 
-
-		with open("Pat1TestCase\\" +str(i)+".sol") as F:
-			SOL = F.read().strip()
-
-		SOL = SOL.split('\n')
-		OUT = OUT.decode().strip().split('\n')
-
-
-		if len(OUT) != len(SOL):
-			Verdict += "-"
-		else:
-
-			LL = 0
-			Pass = True
-			while LL < len(OUT) and Pass:
-				#print(OUT[LL].rstrip(),"|vs|",SOL[LL].rstrip(),"=",OUT[LL].rstrip() == SOL[LL].rstrip())
-				if OUT[LL].rstrip() != SOL[LL].rstrip():
-					Pass = False
-				LL+=1
-			if Pass:
-				Verdict +="P"
-			else:
-				Verdict +="-"
-
-
-	return Verdict
+    return Verdict
